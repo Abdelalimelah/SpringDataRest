ad3df05cc0f81238ea35fcbc85dedee29a9e9abf
Abdelalime Lahbib <abdlalilel12@gmail.com>
2026-05-18 14:19:35 +0100

    Remove .claude directory

--- .claude/worktrees/busy-mcnulty-c9823f/src/main/java/org/SpringDataRest/repository/ProprietaireRepo.java
+++
@@ -1,7 +0,0 @@
-package org.SpringDataRest.repository;
-
-import org.SpringDataRest.modele.Proprietaire;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProprietaireRepo extends CrudRepository<Proprietaire, Long> {
-}
